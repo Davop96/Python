4db840a89dfb658b5320782b4c4a1703469e53b7
Davop96 <davbp@proton.me>
2024-05-11 19:56:22 +0200

    Updated

---
+++ Programs/Products with prices.py
@@ -0,0 +1,33 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Products with prices.py                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dbohoyo- <dbohoyo-@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/05/11 18:49:43 by dbohoyo-          #+#    #+#              #
+#    Updated: 2024/05/11 18:49:43 by dbohoyo-         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+supermarket = {
+	'mango': 2.30,
+	'avocado': 1.90,
+	'tomato': 1.45,
+	'strawberry': 2.30
+}
+
+print('Available products: mango, avocado, tomato, strawberry')
+product = input('Which product do you want to buy? ').lower()
+
+if product in supermarket:
+	while True:
+		try:
+			kilograms = float(input('How many kilograms are you buying? '))
+		except ValueError:
+			print('Invalid input.')
+		else:
+			print('Your total price is', kilograms * supermarket[product], '€')
+			break
+else:
+	print('That product is not available.')
